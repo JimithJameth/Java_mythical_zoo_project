f6b0620ba77a3c1853453638f765c060059566d2
Jamie Martin <jamiemartinmusic@gmail.com>
2017-07-07 14:00:18 +0100

    zoo class setup

---
+++ zoo_managment/Zoo.java
@@ -0,0 +1,26 @@
+package fantasy_zoo;
+import behaviours.*;
+import java.util.ArrayList;
+
+public class FantasyZoo {
+
+  String name;
+  ArrayList<Animals> animal;
+  ArrayList<Enclosures> enclosure;
+
+public FantasyZoo(String name){
+  this.name = name;
+  this.animal = new ArrayList<Animals>();
+  this.enclosure = new ArrayList<Enclosures();
+}
+
+public String getName(){
+  return this.name;
+  }
+public String addEnclosure(){
+  this.enclosure.add(enclosure)
+}
+
+
+}
+
