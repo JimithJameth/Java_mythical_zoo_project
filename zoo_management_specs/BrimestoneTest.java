d4520f11f827909aa564c420c3eff794a6618c53
Jamie Martin <jamiemartinmusic@gmail.com>
2017-07-11 14:24:48 +0100

    Hydra class complete

---
+++ zoo_management_specs/BrimestoneTest.java
@@ -0,0 +1,6 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import zoo_managment.*;
+import behaviours.*;
+
+public class BrimestoneTest {
---
+++ zoo_managment/Hydra.java
@@ -0,0 +1,19 @@
+public class Hydra extends Animal implements Swimmable {
+
+  String hugescream;
+  String aquaticblast;
+
+  public Hydra (String name, int numberoflegs, int age, String type, int levelofdanger,String sleep, int foodLevel,String hugescream, String aquaticblast,Double sellable){
+
+    super(name,numberoflegs,age,type,levelofdanger,sleep,foodLevel,sellable);
+    this.hugescream = hugescream;
+  }
+
+  public String hugescream(){
+    return "SCHRREEEEEAAAAAAAAEEE";
+  }
+
+  public String aquaticblast(){
+    return"The Hydra releases focused torrents of water from the 5 heads"
+  }
+}
